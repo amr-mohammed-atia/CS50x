858575019ab71aed2d7f668fb3f5ac52a14529c2
amr-mohammed-atia <amr.mohammed.atia@gmail.com>
2026-09-15 23:54:56 +0300

    week 2 version 2

--- Week 2/scores.c
+++ Week 2/scores.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
+float average(int numbers[], int input_amount);
+
 int main(void)
 {
-    int scores[3];
+    int input_amount = 0;
     int user_input = 0;
 
-    for (int i = 0; i < 3; i++)
+    do
+    {
+        printf("Enter the amount of scores: ");
+        scanf("%i", &input_amount);
+    }
+    while (input_amount <= 0);
+    
+    int scores[input_amount];
+
+    for (int i = 0; i < input_amount; i++)
     {
         do
         {
@@ -14,5 +25,15 @@ int main(void)
         }
         while (0 >= scores[i] >= 100);
     }
-    printf("Average score is: %.2f\n", (scores[0] + scores[1] + scores[2]) / (float) 3);
+    printf("Average score is: %.2f\n", average(scores, input_amount));
+}
+
+float average(int numbers[], int input_amount)
+{
+    int sum = 0;
+    for (int j = 0; j < input_amount; j++)
+    {
+        sum += numbers[j];
+    }
+    return (sum / input_amount);
 }
