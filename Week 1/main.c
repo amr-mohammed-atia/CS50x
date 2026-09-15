de289f0c9cc6cb84706afdbd79a62fc32783012b
amr-mohammed-atia <amr.mohammed.atia@gmail.com>
2026-09-15 14:39:36 +0300

    stage 2

--- Week 1/main.c
+++ Week 1/main.c
@@ -2,20 +2,35 @@
 
 int main(void)
 {
-    int x;
-    int y;
-    printf("Enter X and Y: ");
-    scanf("%i %i", &x, &y);
-    if (x > y)
+    // int x;
+    // int y;
+    // printf("Enter X and Y: ");
+    // scanf("%i %i", &x, &y);
+    // if (x > y)
+    // {
+    //     printf("X is greater than Y.\n");
+    // }
+    // else if (x < y)
+    // {
+    //     printf("X is smaller than Y.\n");
+    // }
+    // else
+    // {
+    //     printf("X is equal to Y.\n");
+    // }
+    char choice;
+    printf("Do you agree to the terms and conditions? (y/n) ");
+    scanf("%c", &choice);
+    if (choice == 'y' || choice == 'Y')
     {
-        printf("X is greater than Y.\n");
+        printf("Agreed!\n");
     }
-    else if (x < y)
+    else if (choice == 'n' || choice == 'N')
     {
-        printf("X is smaller than Y.\n");
+        printf("Disagreed!\n");
     }
     else
     {
-        printf("X is equal to Y.\n");
+        printf("Invalid input!\n");
     }
 }
