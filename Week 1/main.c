10df0a1606f512dc0014d90ea1d806c8c2233ebc
amr-mohammed-atia <amr.mohammed.atia@gmail.com>
2026-09-15 14:47:10 +0300

    part 2

--- Week 1/main.c
+++ Week 1/main.c
@@ -2,25 +2,35 @@
 
 int main(void)
 {
-    // int x;
-    // int y;
-    // printf("Enter X and Y: ");
-    // scanf("%i %i", &x, &y);
-    // if (x > y)
-    // {
-    //     printf("X is greater than Y.\n");
-    // }
-    // else if (x < y)
-    // {
-    //     printf("X is smaller than Y.\n");
-    // }
-    // else
-    // {
-    //     printf("X is equal to Y.\n");
-    // }
+    // First part:
+    // Initialise the variables
+    int x;
+    int y;
+    // Present user with the request and ask for input
+    printf("Enter X and Y: ");
+    scanf("%i %i", &x, &y);
+    // Condition that handles the comparison of inputs
+    if (x > y)
+    {
+        printf("X is greater than Y.\n");
+    }
+    else if (x < y)
+    {
+        printf("X is smaller than Y.\n");
+    }
+    else
+    {
+        printf("X is equal to Y.\n");
+    }
+    // End of first part
+    
+    // Second part:
+    // Initialise the variable
     char choice;
+    // Send the request to the user and wait for response
     printf("Do you agree to the terms and conditions? (y/n) ");
-    scanf("%c", &choice);
+    scanf(" %c", &choice);
+    // Condition that handles the user's response
     if (choice == 'y' || choice == 'Y')
     {
         printf("Agreed!\n");
@@ -33,4 +43,5 @@ int main(void)
     {
         printf("Invalid input!\n");
     }
+    // End of second part
 }
