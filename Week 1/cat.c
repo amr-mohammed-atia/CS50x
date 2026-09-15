e7cfed9560cc54e991bd0606f63db49a5ebf67bc
amr-mohammed-atia <amr.mohammed.atia@gmail.com>
2026-09-15 17:34:41 +0300

    version 10

--- Week 1/cat.c
+++ Week 1/cat.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int input_with_check(void);
+void meow(void);
 int main(void)
 {
     // Using a while loop for repetition
@@ -17,21 +18,28 @@ int main(void)
         break; //PLACEHOLDER
     }
     printf("How many times do you want the cat to meow? ");
-    int number = input_with_check();
-    // Printing meow
-    for (int i = 0; i < number; i++)
-    {
-        printf("meow\n");
-    }
+    meow();
 }
 
 int input_with_check(void){
+    // Checking inputs to prevent incompatible values
     int number;
     do
     {
         scanf("%i", &number);
     }
     while (number <= 0);
-
+    // Return the correct value for later use
     return number;
+}
+
+void meow(void)
+{
+    // Getting user input
+    int number = input_with_check();
+    // Printing meow
+    for (int i = 0; i < number; i++)
+    {
+        printf("meow\n");
+    }
 }
